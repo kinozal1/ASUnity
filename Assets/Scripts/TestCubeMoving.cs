de77df6a42bb3d2c168bf48481c53a3a0e2fdd18
DESKTOP-SUEF871\SmartUser <ulva95@gmail.com>
2019-01-08 02:51:15 +0300

    Есть сцена с картой, кубик пролетает через некоторые стены, проблема в невидимых стенах. Добавил пустую сцену для меню.

---
+++ Assets/Scripts/TestCubeMoving.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestCubeMoving : MonoBehaviour {
+
+    public float speed=0.5f, tilt;
+   // public GameObject go;
+  //  Rigidbody rb;
+
+    void Start()
+    {
+       // rb = go.GetComponent<Rigidbody>();
+        
+    }
+
+    void FixedUpdate()
+    {
+          transform.GetComponent<Rigidbody>().AddForce(transform.forward*speed, ForceMode.Impulse);
+     
+
+    }
+}
